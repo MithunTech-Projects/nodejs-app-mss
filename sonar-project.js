5cc4625ef4b3e1dae70fbfcad735e380a6838c1c
Alsamad-GH <75019817+Alsamad-GH@users.noreply.github.com>
2021-05-26 21:40:33 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.2.11.21:9090/',
+  serverUrl: 'http://15.206.160.251:9090/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
